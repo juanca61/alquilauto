fa6e7f49fa1f2cc0cbba5a1fb0b4310f86efb382
juanca61 <50838008+juanca61@users.noreply.github.com>
2019-05-20 14:48:35 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.lrmConfig = {
-    serviceUrl: 'https://api.mapbox.com/directions/v5',
+    serviceUrl: 'https://api.mapbox.com/directions/v5?acces_token=pk.eyJ1IjoianVhbmNhcmxvczYxIiwiYSI6ImNqdnduc3N3eTQ4N2U0OXBibGxmdGs4bWYifQ.XBPdvfOWef1ulVbXb97eYg',
     profile: 'mapbox/driving',
 };
